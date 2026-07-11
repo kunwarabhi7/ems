a544377e0f2486710e94f80de6b78a0ed6927b6f
Abhishek <abhishekparmar162@gmail.com>
2026-07-11 16:14:27 +0530

    Convert server from submodule to regular directory

---
+++ server/src/app.js
@@ -0,0 +1,29 @@
+import express from 'express'
+import cors from 'cors'
+import helmet from 'helmet'
+import morgan from 'morgan'
+
+const app = express()
+
+//middleware 
+app.use(helmet());
+
+app.use(
+    cors({
+        origin:"http://localhost:3000",credentials:true
+    })
+)
+
+app.use(express.json())
+
+app.use(express.urlencoded({extended:true}))
+
+app.use(morgan("dev"))
+
+app.get("/",(req,res)=>{
+    res.status(200).json({
+        message:"EMS API is running successfully",success:true 
+    })
+})
+
+export default app;
